a196c9f1385f03d39be851934d550073f52a83d5
fernandotower <fernandotower@gmail.com>
2018-09-27 14:52:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export CORE_AMAZON_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/core_amazon_crud/db/username --output text --query Parameter.Value)"
-  export CORE_AMAZON_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/core_amazon_crud/db/password --output text --query Parameter.Value)"
+  export CORE_AMAZON_CRUD_CORE_API_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/core_amazon_crud/db/username --output text --query Parameter.Value)"
+  export CORE_AMAZON_CRUD_CORE_API_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/core_amazon_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
